f2201a4f22f24e3c727513696517b57e0c8bd55b
Shivam Thakur <shivam.thakur.ug20@nsut.ac.in>
2023-11-09 14:08:36 +0530

    first commit

---
+++ Controllers/ServerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galytix.WebApi.Controllers
+{
+    [ApiController]
+    [Route("/api")]
+    public class ServerController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("ping")]
+        public async Task<IActionResult> Ping()
+        {
+            return Ok("pong");
+        }
+    }
+
+    
+    [ApiController]
+    [Route("api/gwp/avg")]
+    public class CountryGwpController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CalculateAverageGwp([FromBody] GwpRequest request)
+        {
+            
+            return Ok(new
+            {
+                transport = 446001906.1,
+                liability = 634545022.9
+            });
+        }
+}
+    
+}
